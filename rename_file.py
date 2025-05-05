a5d728e27830317fe6e81143273f2400ab8c6746
Kyle Gasser <132257209+kyfgcodes@users.noreply.github.com>
2025-05-05 16:25:54 +0200

    Update rename_file.py
    Added functionality

--- rename_file.py
+++ rename_file.py
@@ -3,19 +3,20 @@
 import os
 
 #Add your folder path here
-folder = r'C:/Users/kfgwo/Music/Anime running'
+folder = r'/home/kyle/Documents/Test'
 
+ep_counter = 1;
 for file_name in os.listdir(folder):
 
+
   source = file_name
-  change = source.replace("[SPOTIFY-DOWNLOADER.COM]", "")
+  change = source.replace(file_name, f"Ep {ep_counter}")
   
   
   os.rename(f'{folder}/{source}',f'{folder}/{change}' )
 
-  
+  ep_counter += 1;
   print(change)
-
 
 
 
